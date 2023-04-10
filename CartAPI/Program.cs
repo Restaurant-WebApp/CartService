aded91248b77dca9d9b2a5ac6ee4995e52cb0f48
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-04-11 00:51:20 +0200

    Ms sql database connection and first migration

--- CartAPI/Program.cs
+++ CartAPI/Program.cs
@@ -1,3 +1,8 @@
+using CartAPI.Data;
+using CartAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddDbContext<CartAppDbContext>( options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
 
 var app = builder.Build();
 
